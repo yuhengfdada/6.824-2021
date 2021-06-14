51cb75945db086513e6c167e26f495833e4a4f65
yuhengfdada <877751954@qq.com>
2021-06-14 10:27:10 +0000

    Lab 2A w/o testing

--- src/raft/raft.go
+++ src/raft/raft.go
@@ -18,14 +18,15 @@ package raft
 //
 
 import (
-//	"bytes"
+	//	"bytes"
+	"math/rand"
 	"sync"
 	"sync/atomic"
+	"time"
 
-//	"6.824/labgob"
+	//	"6.824/labgob"
 	"6.824/labrpc"
 )
-
 
 //
 // as each Raft peer becomes aware that successive log entries are
@@ -50,6 +51,11 @@ type ApplyMsg struct {
 	SnapshotIndex int
 }
 
+type LogEntry struct {
+	Command interface{}
+	Term    int
+}
+
 //
 // A Go object implementing a single Raft peer.
 //
@@ -63,17 +69,29 @@ type Raft struct {
 	// Your data here (2A, 2B, 2C).
 	// Look at the paper's Figure 2 for a description of what
 	// state a Raft server must maintain.
+	// timer
+	timeout       int // election timeout
+	electionTimer *time.Timer
+	// metadata (for convenience)
+	numServers int
+	identity   string
 
+	// persistent
+	currentTerm int
+	votedFor    int
+	log         []LogEntry
+	// volatile
+	commitIndex int
+	lastApplied int
+	// leader
+	nextIndex  []int
+	matchIndex []int
 }
 
 // return currentTerm and whether this server
 // believes it is the leader.
 func (rf *Raft) GetState() (int, bool) {
-
-	var term int
-	var isleader bool
-	// Your code here (2A).
-	return term, isleader
+	return rf.currentTerm, rf.identity == "leader"
 }
 
 //
@@ -91,7 +109,6 @@ func (rf *Raft) persist() {
 	// data := w.Bytes()
 	// rf.persister.SaveRaftState(data)
 }
-
 
 //
 // restore previously persisted state.
@@ -115,7 +132,6 @@ func (rf *Raft) readPersist(data []byte) {
 	// }
 }
 
-
 //
 // A service wants to switch to snapshot.  Only do so if Raft hasn't
 // have more recent info since it communicate the snapshot on applyCh.
@@ -135,65 +151,6 @@ func (rf *Raft) Snapshot(index int, snapshot []byte) {
 	// Your code here (2D).
 
 }
-
-
-//
-// example RequestVote RPC arguments structure.
-// field names must start with capital letters!
-//
-type RequestVoteArgs struct {
-	// Your data here (2A, 2B).
-}
-
-//
-// example RequestVote RPC reply structure.
-// field names must start with capital letters!
-//
-type RequestVoteReply struct {
-	// Your data here (2A).
-}
-
-//
-// example RequestVote RPC handler.
-//
-func (rf *Raft) RequestVote(args *RequestVoteArgs, reply *RequestVoteReply) {
-	// Your code here (2A, 2B).
-}
-
-//
-// example code to send a RequestVote RPC to a server.
-// server is the index of the target server in rf.peers[].
-// expects RPC arguments in args.
-// fills in *reply with RPC reply, so caller should
-// pass &reply.
-// the types of the args and reply passed to Call() must be
-// the same as the types of the arguments declared in the
-// handler function (including whether they are pointers).
-//
-// The labrpc package simulates a lossy network, in which servers
-// may be unreachable, and in which requests and replies may be lost.
-// Call() sends a request and waits for a reply. If a reply arrives
-// within a timeout interval, Call() returns true; otherwise
-// Call() returns false. Thus Call() may not return for a while.
-// A false return can be caused by a dead server, a live server that
-// can't be reached, a lost request, or a lost reply.
-//
-// Call() is guaranteed to return (perhaps after a delay) *except* if the
-// handler function on the server side does not return.  Thus there
-// is no need to implement your own timeouts around Call().
-//
-// look at the comments in ../labrpc/labrpc.go for more details.
-//
-// if you're having trouble getting RPC to work, check that you've
-// capitalized all field names in structs passed over RPC, and
-// that the caller passes the address of the reply struct with &, not
-// the struct itself.
-//
-func (rf *Raft) sendRequestVote(server int, args *RequestVoteArgs, reply *RequestVoteReply) bool {
-	ok := rf.peers[server].Call("Raft.RequestVote", args, reply)
-	return ok
-}
-
 
 //
 // the service using Raft (e.g. a k/v server) wants to start
@@ -215,7 +172,6 @@ func (rf *Raft) Start(command interface{}) (int, int, bool) {
 	isLeader := true
 
 	// Your code here (2B).
-
 
 	return index, term, isLeader
 }
@@ -245,11 +201,8 @@ func (rf *Raft) killed() bool {
 // heartsbeats recently.
 func (rf *Raft) ticker() {
 	for rf.killed() == false {
-
-		// Your code here to check if a leader election should
-		// be started and to randomize sleeping time using
-		// time.Sleep().
-
+		<-rf.electionTimer.C
+		rf.startElection()
 	}
 }
 
@@ -276,9 +229,9 @@ func Make(peers []*labrpc.ClientEnd, me int,
 	// initialize from state persisted before a crash
 	rf.readPersist(persister.ReadRaftState())
 
+	rf.electionTimer = time.NewTimer(time.Duration(rf.timeout+rand.Intn(300)) * time.Millisecond)
 	// start ticker goroutine to start elections
 	go rf.ticker()
-
 
 	return rf
 }
--- src/raft/util.go
+++ src/raft/util.go
@@ -1,6 +1,10 @@
 package raft
 
-import "log"
+import (
+	"log"
+	"math/rand"
+	"time"
+)
 
 // Debugging
 const Debug = false
@@ -10,4 +14,44 @@ func DPrintf(format string, a ...interface{}) (n int, err error) {
 		log.Printf(format, a...)
 	}
 	return
+}
+
+// Timer
+func (rf *Raft) resetElectionTimer() {
+	rf.electionTimer.Stop()
+	rf.electionTimer.Reset(time.Duration(rf.timeout+rand.Intn(300)) * time.Millisecond)
+}
+
+// Role Transition
+// Acquire lock before calling this function.
+func (rf *Raft) changeIdentity(identity string) {
+	rf.identity = identity
+	switch identity {
+	case "follower":
+		rf.resetElectionTimer()
+	case "candidate":
+		rf.currentTerm += 1
+		rf.votedFor = rf.me // vote for self.
+		rf.resetElectionTimer()
+	case "leader":
+		go rf.sendRegularHeartbeats()
+		rf.electionTimer.Stop() // election timeout does not work on leaders.
+	}
+}
+
+// Lock wrappers
+func (rf *Raft) lock(format string, a ...interface{}) {
+	rf.mu.Lock()
+	rf.logger("acquire lock "+format, a...)
+}
+
+func (rf *Raft) unlock(format string, a ...interface{}) {
+	rf.logger("release lock "+format, a...)
+	rf.mu.Unlock()
+}
+
+// Logger
+func (rf *Raft) logger(format string, a ...interface{}) {
+	DPrintf("me: %d, identity:%v,term:%d\n", rf.me, rf.identity, rf.currentTerm)
+	DPrintf(format, a...)
 }
